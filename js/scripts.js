5cc17c77d11198788d2c3fe269f9fefa6913e896
Eduard-Condurache <eduardcondurache96@gmail.com>
2024-07-25 17:48:34 +0200

    Milestone 2 con i bonus

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,40 @@
+const ourTeamHTML = document.getElementById('ourteam-container');
+
 const ourTeam = [
     {
         name: 'Wayne Barnett',
         role: 'Founder & CEO',
-        image: 'wayne-barnett-founder-ceo.jpg'
+        image: 'img/wayne-barnett-founder-ceo.jpg'
     },
 
     {
         name: 'Angela Caroll',
         role: 'Chief Editor',
-        image: 'angela-caroll-chief-editor.jpg'
+        image: 'img/angela-caroll-chief-editor.jpg'
     },
 
     {
         name: 'Walter Gordon',
         role: 'Office Manager',
-        image: 'walter-gordon-office-manager.jpg'
+        image: 'img/walter-gordon-office-manager.jpg'
     },
 
     {
         name: 'Angela Lopez',
         role: 'Social Media Manager',
-        image: 'angela-lopez-social-media-manager.jpg'
+        image: 'img/angela-lopez-social-media-manager.jpg'
     },
 
     {
         name: 'Scott Estrada',
         role: 'Developer',
-        image: 'scott-estrada-developer.jpg'
+        image: 'img/scott-estrada-developer.jpg'
     },
 
     {
         name: 'Barbara Ramos',
         role: 'Graphic Designer',
-        image: 'barbara-ramos-graphic-designer.jpg'
+        image: 'img/barbara-ramos-graphic-designer.jpg'
     },
 ];
 
@@ -57,4 +59,13 @@ for (let key in ourTeam) {
 
 for (let i = 0; i < ourTeam.length; i++) {
     console.log(ourTeam[i].name, ourTeam[i].role, ourTeam[i].image);
+    const teamDetails = `
+        <div class="team-details">
+            <img src="${ourTeam[i].image}">
+            <h3>${ourTeam[i].name}</h3>
+            <p>${ourTeam[i].role}</p>
+        </div>
+    `;
+
+    ourTeamHTML.innerHTML += teamDetails;
 }
