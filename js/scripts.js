6a412a3b68304a0d8e43047d76f931f9e3a51e50
Eduard-Condurache <eduardcondurache96@gmail.com>
2024-07-25 17:13:06 +0200

    Completato Milestone2 (ho fatto un po' di test lol)

--- js/scripts.js
+++ js/scripts.js
@@ -37,3 +37,24 @@ const ourTeam = [
 ];
 
 console.log('ourteam:', ourTeam, ourTeam.length, typeof ourTeam);
+
+console.log(ourTeam[0].name, ourTeam[0].role, ourTeam[0].image);
+
+console.log(ourTeam[1].name, ourTeam[1].role, ourTeam[1].image);
+
+console.log(ourTeam[2].name, ourTeam[2].role, ourTeam[2].image);
+    
+console.log(ourTeam[3].name, ourTeam[3].role, ourTeam[3].image);
+
+console.log(ourTeam[4].name, ourTeam[4].role, ourTeam[4].image);
+
+console.log(ourTeam[5].name, ourTeam[5].role, ourTeam[5].image);
+
+for (let key in ourTeam) {
+    console.log(ourTeam[key]);
+
+}
+
+for (let i = 0; i < ourTeam.length; i++) {
+    console.log(ourTeam[i].name, ourTeam[i].role, ourTeam[i].image);
+}
